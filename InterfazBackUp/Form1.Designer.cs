0f04600c9c0d7012c1b5e82aec3ad2ed22182a19
Alex <aarenzana@gmail.com>
2020-10-25 20:21:19 +0100

    Inicial

--- InterfazBackUp/Form1.Designer.cs
+++ InterfazBackUp/Form1.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.buttonExpandir = new System.Windows.Forms.Button();
+            this.buttonContraer = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +47,62 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Seleccione los archivos a guardar:";
             // 
+            // treeView1
+            // 
+            this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.treeView1.Location = new System.Drawing.Point(16, 42);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(223, 367);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(245, 42);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(425, 367);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // buttonExpandir
+            // 
+            this.buttonExpandir.Location = new System.Drawing.Point(16, 432);
+            this.buttonExpandir.Name = "buttonExpandir";
+            this.buttonExpandir.Size = new System.Drawing.Size(109, 34);
+            this.buttonExpandir.TabIndex = 3;
+            this.buttonExpandir.Text = "Expandir Todo";
+            this.buttonExpandir.UseVisualStyleBackColor = true;
+            this.buttonExpandir.Click += new System.EventHandler(this.buttonExpandir_Click);
+            // 
+            // buttonContraer
+            // 
+            this.buttonContraer.Location = new System.Drawing.Point(131, 432);
+            this.buttonContraer.Name = "buttonContraer";
+            this.buttonContraer.Size = new System.Drawing.Size(109, 34);
+            this.buttonContraer.TabIndex = 4;
+            this.buttonContraer.Text = "Contraer Todo";
+            this.buttonContraer.UseVisualStyleBackColor = true;
+            this.buttonContraer.Click += new System.EventHandler(this.buttonContraer_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "documents_folder.ico");
+            this.imageList1.Images.SetKeyName(1, "fileinterfaces.ico");
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(844, 487);
+            this.Controls.Add(this.buttonContraer);
+            this.Controls.Add(this.buttonExpandir);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -58,6 +115,11 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button buttonExpandir;
+        private System.Windows.Forms.Button buttonContraer;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
--- InterfazBackUp/Form1.cs
+++ InterfazBackUp/Form1.cs
@@ -15,6 +15,44 @@ namespace InterfazBackUp
         public Form1()
         {
             InitializeComponent();
+            CrearTreeView();
+        }
+
+        public void CrearTreeView()
+        {
+            treeView1.Nodes.Clear();
+            treeView1.ImageList = imageList1;
+            treeView1.Nodes.Add("TreeView", "Personas", 0);
+            treeView1.Nodes[0].Nodes.Add("1", "Alejandro", 1);
+            treeView1.Nodes[0].Nodes.Add("1", "Lara", 1);
+            treeView1.Nodes[0].Nodes.Add("1", "Sofía", 1);
+            treeView1.Nodes[0].Nodes.Add("1", "Emma", 1);
+        }
+
+        private void buttonExpandir_Click(object sender, EventArgs e)
+        {
+            treeView1.ExpandAll();
+        }
+
+        private void buttonContraer_Click(object sender, EventArgs e)
+        {
+            treeView1.CollapseAll();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (treeView1.Nodes.Count != 0) {
+                MessageBox.Show(e.Node.Text);
+              
+                
+            }
         }
     }
+
+ 
 }
